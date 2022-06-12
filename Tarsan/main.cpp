76e48a78b81f202635ca94abbc5d937347c7f43d
Jakub Charvát <charvj12@fit.cvut.cz>
2022-06-12 04:06:47 +0200

    Initial Commit

---
+++ Tarsan/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Tarsan
+//
+//  Created by Jakub Charvát on 12.06.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
